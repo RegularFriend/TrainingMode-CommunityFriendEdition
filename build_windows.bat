226b6f6a0a14cf55bff2c5135b1d8c37b9c90bc5
Alex Harrison <alexander.harrison.r@gmail.com>
2025-02-12 10:58:32 -0700

    add powershield event to build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -66,6 +66,10 @@ copy "dats\wavedash.dat" "build\wavedash.dat"
 "MexTK/MexTK.exe" -ff -i "src/wavedash.c" -b "build" -s evFunction -dat "build/wavedash.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'wavedash.c' & goto cleanup)
 "MexTK/MexTK.exe" -trim "build/wavedash.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
 
+echo build powershield event
+"MexTK/MexTK.exe" -ff -i "src/powershield.c" -b "build" -s evFunction -dat "build/powershield.dat" -t "MexTK/evFunction.txt" -q -ow -l "MexTK/melee.link" -op 2 || ( echo ERROR: Failed to compile 'powershield.c' & goto cleanup)
+"MexTK/MexTK.exe" -trim "build/powershield.dat" || ( echo ERROR: Dat file trimming failed & goto cleanup )
+
 echo BUILD ASM FILES --------------------------------------------------------
 
 del "Additional ISO Files\codes.gct"
@@ -108,6 +112,7 @@ gc_fst fs TM-CE.iso ^
 	insert TM\lcancel.dat build\lcancel.dat ^
 	insert TM\ledgedash.dat build\ledgedash.dat ^
 	insert TM\wavedash.dat build\wavedash.dat ^
+	insert TM\powershield.dat build\powershield.dat ^
 	insert codes.gct build\codes.gct ^
 	insert Start.dol build\Start.dol ^
 	insert opening.bnr build\opening.bnr
