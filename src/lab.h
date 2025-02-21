db61f94a4eb8aa39840605afb9766560de307436
Alex Harrison <alexander.harrison.r@gmail.com>
2025-02-21 13:24:35 -0700

    alter speeds

--- src/lab.h
+++ src/lab.h
@@ -863,8 +863,8 @@ static char *LabOptions_ModelDisplay[] = {"On", "Stage Only", "Characters Only"}
 static const bool LabValues_CharacterModelDisplay[] = {true, false, true};
 static const bool LabValues_StageModelDisplay[] = {true, true, false};
 
-static float LabOptions_GameSpeeds[] = {1.f, 4.f/5.f, 3.f/4.f, 2.f/3.f, 1.f/2.f, 1.f/4.f};
-static char *LabOptions_GameSpeedText[] = {"1", "4/5", "3/4", "2/3", "1/2", "1/4"};
+static float LabOptions_GameSpeeds[] = {1.f, 5.f/6.f, 2.f/3.f, 1.f/2.f, 1.f/4.f};
+static char *LabOptions_GameSpeedText[] = {"1", "5/6", "2/3", "1/2", "1/4"};
 
 static EventOption LabOptions_General[OPTGEN_COUNT] = {
     {
